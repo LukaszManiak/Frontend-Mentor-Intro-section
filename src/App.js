e73e259547c54f384785592830760b9cc0422068
Lukasz <lukasz.maniak22@gmail.com>
2023-10-20 21:04:25 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+//  Features Company Careers About  Login Register 
+      
+//       Make remote work 
+      
+//       Get your team
+//     in sync, no matter your location. Streamline processes, create team rituals,
+//     and watch productivity soar. 
+    
+//     Learn more 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
     </div>
   );
 }
